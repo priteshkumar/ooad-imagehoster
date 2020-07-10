29959f78c0c477e2cac88c3ecb2a582ecdec72ab
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-07-10 19:33:48 +0530

    add CommentService/CommentRepository beans

---
+++ src/main/java/ImageHoster/repository/CommentRepository.java
@@ -0,0 +1,23 @@
+package ImageHoster.repository;
+
+import ImageHoster.model.Comment;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceUnit;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CommentRepository {
+
+  @PersistenceUnit(unitName = "imageHoster")
+  private EntityManagerFactory emf;
+
+  public void addComment(Comment comment) {
+    EntityManager entityManager = emf.createEntityManager();
+    EntityTransaction entityTransaction = entityManager.getTransaction();
+    entityTransaction.begin();
+    entityManager.persist(comment);
+    entityTransaction.commit();
+  }
+}
